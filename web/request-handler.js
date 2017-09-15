eb675be5e8fac8157f7dcd701a9273c4d0cebf93
Clara Cho <clara.cho@gmail.com>
2017-09-15 13:16:00 -0700

    load index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,34 @@
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
+const mime = require('mime-types'); 
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('Serving request type ' + req.method + ' for url ' + req.url);
+
+  let statusCode;
+  let headers = http.headers;
+
+  if (req.method === 'GET') {
+    let fileUrl = (req.url === '/') 
+      ? archive.paths.siteAssets + '/index.html' 
+      : archive.paths.siteAssets + req.url; 
+    // fetch files
+    fs.readFile(fileUrl, (err, data) => {
+      if (err) {
+        console.log('error');
+        statusCode = 404;
+        headers['Content-Type'] = 'text/plain';
+        res.writeHead(statusCode, headers);
+        res.end();
+      } else {
+        statusCode = 200;
+        headers['Content-Type'] = mime.lookup(fileUrl);
+        res.writeHead(statusCode, headers);
+        res.end(data);
+      }
+    });
+  }
 };
